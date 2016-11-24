fefc6c9ad15868a75dea04bf3fd0f42e4bef1d92
Casper F. Overgaard <cfo1994@hotmail.com>
2016-11-24 14:08:34 +0100

    Mine forrige commits er blevet slettet

---
+++ sdktest.js
@@ -0,0 +1,118 @@
+/**
+ * Created by C.F. Overgaard on 14/11/2016.
+ */
+
+var SDK = {
+
+    serverURL: "https://localhost:8000",
+
+    request: function (options, cb) {
+
+        var headers = {};
+        if (options.headers) {
+            Object.keys(options.headers).forEach(function (h) {
+                headers[h] = (typeof options.headers[h] === 'object') ? JSON.stringify(options.headers[h]) : options.headers[h];
+            });
+        }
+
+        //Perform XHR
+        $.ajax({
+            url: SDK.serverURL + options.url,
+            method: options.method,
+            contentType: "application/json",
+            headers: headers,
+            dataType: "json",
+            data: JSON.stringify(options.data),
+            xhrFields: {withCredentials: true },
+            success: function (data, status, xhr) {
+                cb(null, data, status, xhr);
+            },
+            error: function (xhr, status, errorThrown) {
+                cb({xhr: xhr, status: status, error: errorThrown});
+            }
+        });
+    },
+
+    User: {
+        create: function(data, cb) {
+            SDK.request({method: "POST", url: "/createuser", data: data}, cb);
+        },
+
+        current:function () {
+            return SDK.Storage.load("user");
+        },
+    },
+
+
+    Book: {
+        getAll: function (cb) {
+            SDK.request({
+                method: "GET", url: "/getbooks",
+            }, cb);
+        },
+    },
+
+    Ad: {
+        getAds: function (cb) {
+            SDK.request({
+                method: "GET", url: "/getads"
+            }, cb);
+        },
+
+        getMyAds: function (cb) {
+            SDK.request({
+                method: "GET", url: "/getmyads"
+            }, cb);
+        },
+    },
+
+    logOut:function() {
+        SDK.Storage.remove("tokenId");
+        SDK.Storage.remove("userId");
+        SDK.Storage.remove("user");
+    },
+
+    login: function (username, password, cb) {
+        this.request({
+            data: {
+                username: username,
+                password: password
+            },
+            url: "/login",
+            method: "POST",
+        }, function (err, data) {
+
+            if (err) return cb(err);
+
+            SDK.Storage.persist("sessionId", data.sessionid);
+            SDK.Storage.persist("userId", data.userId);
+            SDK.Storage.persist("username", data.username);
+
+            cb(null, data);
+
+        });
+    },
+
+    Storage: {
+        prefix: "brugtbogsbors",
+        persist: function (key, value) {
+            window.localStorage.setItem(this.prefix + key, (typeof value === 'object') ? JSON.stringify(value) : value)
+        },
+        load: function (key) {
+            var val = window.localStorage.getItem(this.prefix + key);
+            try {
+                return JSON.parse(val);
+            }
+            catch (e) {
+                return val;
+            }
+        },
+        remove: function (key) {
+            window.localStorage.removeItem(this.prefix + key);
+        }
+    },
+
+
+
+};
+
---
+++ user.page.js
@@ -0,0 +1,52 @@
+/**
+ * Created by C.F. Overgaard on 21/11/2016.
+ */
+$(document).ready(function () {
+
+
+    SDK.Ad.getAds(function(err, ads){
+        if(err) throw(err);
+
+        var $adsTableBody = $("#adsTableBody");
+        ads.forEach(function (ad) {
+
+            $adsTableBody.append(
+                "<tr>" +
+                "<td>" + ad.isbn + "</td>" +
+                "<td>" + ad.bookTitle + "</td>" +
+                "<td>" + ad.bookAuthor + "</td>" +
+                "<td>" + ad.bookEdition + "</td>" +
+                "<td>" + ad.price + "</td>" +
+                "<td>" + ad.comment + "</td>" +
+                "<td>" + ad.rating + "</td>" +
+                "</tr>");
+        });
+
+    });
+
+    SDK.Ad.getMyAds(function(err, ads){
+        if(err) throw(err);
+
+        var $myAdsTableBody = $("#myAdsTableBody");
+        ads.forEach(function (ad){
+
+            $myAdsTableBody.append(
+                "<tr>" +
+                "<td>" + ad.isbn + "</td>" +
+                "<td>" + ad.bookTitle + "</td>" +
+                "<td>" + ad.bookAuthor + "</td>" +
+                "<td>" + ad.bookEdition + "</td>" +
+                "<td>" + ad.price + "</td>" +
+                "<td>" + ad.comment + "</td>" +
+                "<td>" + ad.rating + "</td>" +
+                "</tr>");
+        });
+
+    });
+
+    $("#logOutLink").on("click", function () {
+        SDK.logOut();
+        window.location.href = "/index.html";
+
+    });
+});
